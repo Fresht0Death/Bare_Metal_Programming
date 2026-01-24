8c20a242257899aed25b3f5276dd919ace144baf
Jahmai Cabrera <127389693+Fresht0Death@users.noreply.github.com>
2026-01-24 11:33:10 -0800

    Add files via upload
    Used what I know about uart to help me debug my board. The goal was to get the LED to turn on for 5 seconds when the button is pressed and say off otherwise using my new delay() function.

---
+++ systick.h
@@ -0,0 +1,15 @@
+/*
+ * systick.h
+ *
+ *  Created on: Jan 24, 2026
+ *      Author: Freshmai
+ */
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+#include "stm32f4xx.h"
+
+
+void systickDelayMS(int delay);
+
+#endif /* SYSTICK_H_ */
