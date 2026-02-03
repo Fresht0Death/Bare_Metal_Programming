64b02a28ddb922f9e97cc1e921542b7031b9dbed
Jahmai Cabrera <127389693+Fresht0Death@users.noreply.github.com>
2026-02-03 14:45:48 -0800

    Add files via upload
    Updated tim.h, tim.c. new function in tim.c includes to capture when flag is set and output the change in timer using debug live expressions.

---
+++ LearningVideoCode/TimerInputCaptureDriver/systick.h
@@ -0,0 +1,14 @@
+/*
+ * systick.h
+ *
+ *  Created on: Jan 22, 2026
+ *      Author: Freshmai
+ */
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+void systickDelayMS(int delay);
+
+
+#endif /* SYSTICK_H_ */
