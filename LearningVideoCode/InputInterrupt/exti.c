a157123a009858f0a89fd1581d545ab310dcc1ec
Jahmai Cabrera <127389693+Fresht0Death@users.noreply.github.com>
2026-02-10 21:17:39 -0800

    Add files via upload
    We include two new files exti.c, exti.c, and new main code which executes the functions defined in these files. All other files are in the repository and have been previously mentioned in their respective files.

---
+++ LearningVideoCode/InputInterrupt/exti.c
@@ -0,0 +1,76 @@
+/*
+ * exti.c
+ *
+ *  Created on: Feb 10, 2026
+ *      Author: Freshmai
+ */
+
+
+// exti.c
+
+#include "exti.h"
+#include "stm32f4xx.h"
+
+#define GPIOCEN       (1U<<2)
+#define SYSCFGEN      (1U<<14)
+
+
+void pc13_exti_init(void)
+{
+	/* Best Practice: Disable global interupt   */
+	__disable_irq();
+
+	/* Enable clock access for GPIOC */
+	RCC->AHB1ENR |= GPIOCEN;
+
+	/* Best practice: set PC13 as input */
+		// check mode register in ref manual
+		// we look for MODERx and see what bits
+		// correspond to pc13 and look at truth
+		// table for how to config as input
+	GPIOC->MODER &=~(1U<<26);
+	GPIOC->MODER &=~(1U<<27);
+
+
+
+	/* Enable clock access to SYSCFG */
+		// what bus is SYSCFG connected to?
+		// go to BUS name enable register to check
+		// check which bit enable clock access to SYSCFG
+	RCC->APB2ENR |= SYSCFGEN;
+
+	/* Select PORTC for EXTI13 */
+		// go to SYSCFG exti control register
+		// note the seperation and the truth table
+		// look for EXTI13 in the xth register which holds this
+		// then set the bit in order to connect to PAC[x] based on the truth table
+	SYSCFG->EXTICR[3] |= (1<<5); // simiarly to our alt-func register we use an array to access the nth EXTICRn by using n-1 based indexing
+
+
+
+
+	/* Unmask/Uncover EXTI13 */
+		// go to interrupt mask register(EXTI_IMR)
+		// we are interested in line 13 so we look for
+		// the which corresponds to this
+		// look at the truth table about how to unmask it
+	EXTI->IMR |= (1U<<13);
+
+
+	/* Select falling edge trigger */
+		// go to falling trigger selection register for EXTI
+		// look at the truth table to enable falling trigger
+		// find the correspinding bit which fits for 13
+	EXTI->FTSR |= (1U<<13);
+
+	/* Enable exti line in NVIC */
+		// use a function that comes in our stm32f4xx.h file
+	NVIC_EnableIRQ(EXTI15_10_IRQn/*passed interrupt here, this is a tough one*/);
+
+
+	/* Enable global interrupts */
+	__enable_irq();
+
+
+
+}
---
+++ LearningVideoCode/InputInterrupt/tim.h
@@ -0,0 +1,20 @@
+/*
+ * tim.h
+ *
+ *  Created on: Jan 27, 2026
+ *      Author: Freshmai
+ */
+
+#ifndef TIM_H_
+#define TIM_H_
+
+void tim2_1hz_init(void);
+#define SR_UIF		(1U<<0)
+void tim2_output_compare_pa5(void);
+void tim3_pa6_input_capture(void);
+// include new macro in tim.h
+// this will be the symbolic bit for the status register
+#define SR_CC1IF      (1U<<1)
+
+
+#endif /* TIM_H_ */
