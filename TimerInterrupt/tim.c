71dcb5f20f68f94039be4453d3263d790e27dbac
Jahmai Cabrera <127389693+Fresht0Death@users.noreply.github.com>
2026-03-07 16:03:06 -0800

    Add files via upload

---
+++ TimerInterrupt/tim.c
@@ -0,0 +1,80 @@
+/*
+ * tim.c
+ *
+ *  Created on: Jan 27, 2026
+ *      Author: Freshmai
+ */
+
+
+#include "stm32f4xx.h"
+
+#define TIM2EN				(1U<<0)
+#define CR1_CEN				(1U<<0)
+#define DIER_UIE     		(1U<<0)
+
+
+
+
+
+void tim2_1hz_init(void)
+{
+	/* Enable clock access to tim2 */
+	RCC->APB1ENR |= TIM2EN;
+
+
+	/* Set prescale value */
+	TIM2->PSC = 1600 - 1; // 16 000 000 000 / 1600 = 10 000
+
+
+	/* Set auto reload value */
+	TIM2->ARR = 10000 - 1; // 10 000 / 10 000 = 1hz
+
+	/* Clear counter */
+	TIM2->CNT = 0;
+
+	/* Enable timer */
+	TIM2->CR1 |= CR1_CEN;
+
+
+
+}
+
+
+void tim2_1hz_interrupt_init(void)
+{
+	/* Enable clock access to tim2 */
+	RCC->APB1ENR |= TIM2EN;
+
+
+	/* Set prescale value */
+	TIM2->PSC = 1600 - 1; // 16 000 000 000 / 1600 = 10 000
+
+
+	/* Set auto reload value */
+	TIM2->ARR = 10000 - 1; // 10 000 / 10 000 = 1hz
+
+	/* Clear counter */
+	TIM2->CNT = 0;
+
+	/* Enable timer */
+	TIM2->CR1 |= CR1_CEN;
+
+
+	/* enable interrupt for tim2 */
+	// dier = Direct Memory Access/ Interrupt Enable register
+	TIM2->DIER |= DIER_UIE;
+
+	// Enable tim2 interrupt via the nvic table
+	NVIC_EnableIRQ(TIM2_IRQn);
+
+
+
+
+
+
+}
+
+
+
+
+
