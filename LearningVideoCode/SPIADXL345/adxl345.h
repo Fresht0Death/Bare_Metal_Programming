09cc0cb25da8988f699e92a7ffd568b646005758
Jahmai Cabrera <127389693+Fresht0Death@users.noreply.github.com>
2026-06-11 10:06:18 -0700

    Add files via upload
    updated adxl345 source and header files, new spi source files, new main file which uses spi protocol with the adxl345

---
+++ LearningVideoCode/SPIADXL345/adxl345.h
@@ -0,0 +1,28 @@
+/*
+ * adxl345.h
+ *
+ *  Created on: May 26, 2026
+ *      Author: Freshmai
+ */
+
+#ifndef ADXL345_H_
+#define ADXL345_H_
+
+#include <stdint.h>
+#include "spi.h"
+
+#define DEVID_R					(0x00)
+#define DEVICE_ADDR				(0x53)
+#define DATA_FORMAT_R			(0x31)
+#define DATA_START_ADDR			(0x32)
+#define POWER_CTRL_R			(0x2D)
+
+#define FOUR_G					(0x01)
+#define ADXL345_POWER_RESET    (0x00)
+#define SET_MEASURE_B			(0x08)
+
+
+void adxl_init(void);
+void adxl_read(uint8_t address, uint8_t * rxdata);
+
+#endif /* ADXL345_H_ */
